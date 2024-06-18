afc164a2be39e1953baa1c8fd52014eb80b043c4
RamyaU <ramyayadav2016@gmail.com>
2024-06-18 15:20:33 +0530

    creation and registering db context

--- FullStack-EmployeeManagementSystem/EmployeeManagement/EmployeeManagement/Program.cs
+++ FullStack-EmployeeManagementSystem/EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,5 +1,11 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDBContext>(
+    options => options.UseInMemoryDatabase("ApplicationDB"));
 
 app.MapGet("/", () => "Hello World!");
 
