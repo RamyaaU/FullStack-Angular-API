93f372ec945b853fbd88e6e9fe77223221d33bdc
RamyaU <ramyayadav2016@gmail.com>
2024-06-19 15:46:44 +0530

    implementation of create, update, delete and get api's

--- FullStack-EmployeeManagementSystem/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
+++ FullStack-EmployeeManagementSystem/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -15,11 +15,49 @@ namespace EmployeeManagement.Controller
             _employeeRepository = employeeRepository;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllAsync()
+        {
+            var allEmployees = await _employeeRepository.GetAllAsync();
+            return Ok(allEmployees);
+        }
+
         [HttpPost]
         public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
         {
             await _employeeRepository.AddEmployeeAsync(employee);
-            return Created();
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployeeById(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeByAsync(int id, Employee employee)
+        {
+            if( id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            await _employeeRepository.UpdateEmployeeAsync(employee);
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
         }
     }
 }
