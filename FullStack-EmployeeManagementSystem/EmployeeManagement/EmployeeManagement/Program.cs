364ee8358c01232988179ab696a60ddb4ae982a0
RamyaU <ramyayadav2016@gmail.com>
2024-06-19 14:31:33 +0530

    configuring swagger in app

--- FullStack-EmployeeManagementSystem/EmployeeManagement/EmployeeManagement/Program.cs
+++ FullStack-EmployeeManagementSystem/EmployeeManagement/EmployeeManagement/Program.cs
@@ -3,25 +3,42 @@ using EmployeeManagement.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
+// Register services before building the app
 builder.Services.AddDbContext<ApplicationDBContext>(
     options => options.UseInMemoryDatabase("ApplicationDB"));
 
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// Add CORS policy
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("MyCors", builder =>
+    options.AddPolicy("MyCors", policyBuilder =>
     {
-        builder.WithOrigins("https://localhost:4200")
-        .AllowAnyMethod()
-        .AllowAnyHeader();
+        policyBuilder.WithOrigins("https://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
     });
 });
 
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    });
+}
+
+app.UseCors("MyCors");
+
+app.MapControllers(); // Map controllers
 
 app.MapGet("/", () => "Hello World!");
 
